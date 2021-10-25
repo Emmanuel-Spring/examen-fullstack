0eec05dfcf63ff9f78fba1a5ba0f52c5c4c98ec4
Emmanuel-spring <e.nieto@alumnos.santotomas.cl>
2021-10-25 12:04:08 -0300

    Project Config

---
+++ src/main/java/com/talentyco/springboot/SpringbootsistemafacturacionApplication.java
@@ -0,0 +1,13 @@
+package com.talentyco.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsistemafacturacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsistemafacturacionApplication.class, args);
+	}
+
+}
